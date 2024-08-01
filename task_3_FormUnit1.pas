518a5a200d589243a6c4000940d54a3a86d575b8
nookie48 <domnin.andrey@gmail.com>
2024-08-01 11:53:29 +0500

    task_3_FormUnit1.pas

---
+++ task_3_FormUnit1.pas
@@ -0,0 +1,109 @@
+unit FormUnit1;
+
+interface
+
+uses
+  Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  Vcl.AppEvnts, Vcl.StdCtrls, IdHTTPWebBrokerBridge, IdGlobal, Web.HTTPApp,
+  Vcl.Mask, Vcl.ExtCtrls, Vcl.DBCtrls, Data.DB, Data.Win.ADODB;
+
+type
+  TForm1 = class(TForm)
+    ButtonStart: TButton;
+    ButtonStop: TButton;
+    EditPort: TEdit;
+    Label1: TLabel;
+    ApplicationEvents1: TApplicationEvents;
+    ButtonOpenBrowser: TButton;
+    ADOConnection1: TADOConnection;
+    ADOQuery1: TADOQuery;
+    ADOQuery1id: TAutoIncField;
+    ADOQuery1full_name: TWideStringField;
+    ADOQuery1birth_date: TDateField;
+    ADOQuery1internal_phone: TWideStringField;
+    ADOQuery1email: TWideStringField;
+    DataSource1: TDataSource;
+    Label2: TLabel;
+    DBEdit1: TDBEdit;
+    Label3: TLabel;
+    DBEdit2: TDBEdit;
+    Label4: TLabel;
+    DBEdit3: TDBEdit;
+    Label5: TLabel;
+    DBEdit4: TDBEdit;
+    procedure FormCreate(Sender: TObject);
+    procedure ApplicationEvents1Idle(Sender: TObject; var Done: Boolean);
+    procedure ButtonStartClick(Sender: TObject);
+    procedure ButtonStopClick(Sender: TObject);
+    procedure ButtonOpenBrowserClick(Sender: TObject);
+  private
+    FServer: TIdHTTPWebBrokerBridge;
+    procedure StartServer;
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+uses
+{$IFDEF MSWINDOWS}
+  WinApi.Windows, Winapi.ShellApi,
+{$ENDIF}
+  System.Generics.Collections;
+
+procedure TForm1.ApplicationEvents1Idle(Sender: TObject; var Done: Boolean);
+begin
+  ButtonStart.Enabled := not FServer.Active;
+  ButtonStop.Enabled := FServer.Active;
+  EditPort.Enabled := not FServer.Active;
+end;
+
+procedure TForm1.ButtonOpenBrowserClick(Sender: TObject);
+{$IFDEF MSWINDOWS}
+var
+  LURL: string;
+{$ENDIF}
+begin
+  StartServer;
+{$IFDEF MSWINDOWS}
+  LURL := Format('http://localhost:%s', [EditPort.Text]);
+  ShellExecute(0,
+        nil,
+        PChar(LURL), nil, nil, SW_SHOWNOACTIVATE);
+{$ENDIF}
+end;
+
+procedure TForm1.ButtonStartClick(Sender: TObject);
+begin
+  StartServer;
+end;
+
+procedure TForm1.ButtonStopClick(Sender: TObject);
+begin
+  FServer.Active := False;
+  FServer.Bindings.Clear;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  FServer := TIdHTTPWebBrokerBridge.Create(Self);
+end;
+
+procedure TForm1.StartServer;
+begin
+  if not FServer.Active then
+  begin
+    FServer.Bindings.Clear;
+    FServer.DefaultPort := StrToInt(EditPort.Text);
+    FServer.Active := True;
+  end;
+end;
+
+end.
